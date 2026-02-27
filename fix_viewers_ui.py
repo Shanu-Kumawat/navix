bada38a30157271c0126e693a3c358f4bb756e26
Shanu Kumawat <shanu.kumawat.dev@gmail.com>
2026-02-28 04:36:11 +0530

    chore: remove dirty migration scripts from phase 1

--- fix_viewers_ui.py
+++
@@ -1,8 +0,0 @@
-with open("src/ui/Viewers3DUI.cpp", "r") as f:
-    text = f.read()
-
-text = text.replace("ImVec2 viewportSize =", "glm::dvec2 viewportSize =")
-text = text.replace("ImVec2(availableSize.x", "glm::dvec2(availableSize.x")
-
-with open("src/ui/Viewers3DUI.cpp", "w") as f:
-    f.write(text)
